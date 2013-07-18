f003547d88c48594357a926a4790b4a835119223
Andris Reinman <andris@kreata.ee>
2013-07-18 09:28:01 +0300

    Added tests

---
+++ test/mo-parser.js
@@ -0,0 +1,30 @@
+var gettextParser = require(".."),
+    fs = require("fs");
+
+module.exports["UTF-8"] = {
+    setUp: function(callback){
+        this.mo = fs.readFileSync(__dirname + "/fixtures/utf8.mo");
+        this.json = JSON.parse(fs.readFileSync(__dirname + "/fixtures/utf8-parsed-mo.json", "utf-8"));
+        callback();
+    },
+
+    parse: function(test){
+        var parsed = gettextParser.mo.parse(this.mo);
+        test.deepEqual(parsed, this.json);
+        test.done();
+    }
+}
+
+module.exports["Latin13"] = {
+    setUp: function(callback){
+        this.mo = fs.readFileSync(__dirname + "/fixtures/latin13.mo");
+        this.json = JSON.parse(fs.readFileSync(__dirname + "/fixtures/latin13-parsed-mo.json", "utf-8"));
+        callback();
+    },
+
+    parse: function(test){
+        var parsed = gettextParser.mo.parse(this.mo);
+        test.deepEqual(parsed, this.json);
+        test.done();
+    }
+}
---
+++ test/po-compiler.js
@@ -0,0 +1,28 @@
+var gettextParser = require(".."),
+    fs = require("fs");
+
+module.exports["UTF-8"] = {
+    setUp: function(callback){
+        this.json = JSON.parse(fs.readFileSync(__dirname + "/fixtures/utf8-parsed-po.json", "utf-8"));
+        this.po = fs.readFileSync(__dirname + "/fixtures/utf8.po", "utf-8");
+        callback();
+    },
+    compile: function(test){
+        var compiled = gettextParser.po.compile(this.json);
+        test.equal(compiled, this.po)
+        test.done();
+    }
+}
+
+module.exports["Latin-8"] = {
+    setUp: function(callback){
+        this.json = JSON.parse(fs.readFileSync(__dirname + "/fixtures/latin13-parsed-po.json", "utf-8"));
+        this.po = fs.readFileSync(__dirname + "/fixtures/latin13.po");
+        callback();
+    },
+    compile: function(test){
+        var compiled = gettextParser.po.compile(this.json);
+        test.deepEqual(Array.prototype.slice.call(compiled), Array.prototype.slice.call(this.po));
+        test.done();
+    }
+}
