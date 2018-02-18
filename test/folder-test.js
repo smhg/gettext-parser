82355c25f472f028b00c206c9b1ed90cb51cbe2e
smhg <sam.hauglustaine@gmail.com>
2018-02-18 11:16:42 +0100

    add white space and special character folding tests

--- test/folder-test.js
+++ test/folder-test.js
@@ -7,7 +7,7 @@ var expect = chai.expect;
 chai.config.includeStack = true;
 
 describe('Folding tests', function () {
-  it('Short line, no folding', function () {
+  it('No folding', function () {
     var line = 'abc def ghi';
     var folded = sharedFuncs.foldLine(line);
 
@@ -15,7 +15,7 @@ describe('Folding tests', function () {
     expect(folded.length).to.equal(1);
   });
 
-  it('Short line, force fold with newline', function () {
+  it('Force fold with newline', function () {
     var line = 'abc \\ndef \\nghi';
     var folded = sharedFuncs.foldLine(line);
 
@@ -24,7 +24,7 @@ describe('Folding tests', function () {
     expect(folded.length).to.equal(3);
   });
 
-  it('Long line', function () {
+  it('Fold at default length', function () {
     var expected = ['Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum pretium ',
       'a nunc ac fringilla. Nulla laoreet tincidunt tincidunt. Proin tristique ',
       'vestibulum mauris non aliquam. Vivamus volutpat odio nisl, sed placerat ',
@@ -36,5 +36,23 @@ describe('Folding tests', function () {
     var folded = sharedFuncs.foldLine(expected.join(''));
     expect(folded).to.deep.equal(expected);
     expect(folded.length).to.equal(7);
+  });
+
+  it('Force fold white space', function () {
+    var line = 'abc def ghi';
+    var folded = sharedFuncs.foldLine(line, 5);
+
+    expect(line).to.equal(folded.join(''));
+    expect(folded).to.deep.equal(['abc ', 'def ', 'ghi']);
+    expect(folded.length).to.equal(3);
+  });
+
+  it('Force fold special character', function () {
+    var line = 'abcdef--ghi';
+    var folded = sharedFuncs.foldLine(line, 5);
+
+    expect(line).to.equal(folded.join(''));
+    expect(folded).to.deep.equal(['abcde', 'f--', 'ghi']);
+    expect(folded.length).to.equal(3);
   });
 });
