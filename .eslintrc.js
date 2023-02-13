017acabed6d23c6e78266fb363a41cf29c3d35b3
Volodymyr Khytskyi <83341060+vkhytskyi-allegro@users.noreply.github.com>
2023-02-13 14:31:20 +0100

    rc0

--- .eslintrc.js
+++ .eslintrc.js
@@ -13,6 +13,7 @@ module.exports = {
     'ecmaVersion': 2018
   },
   'rules': {
-    'semi': ['error', 'always']
+    'semi': ['error', 'always'],
+    'camelcase': 'off'
   }
 }
--- test/po-parser-test.js
+++ test/po-parser-test.js
@@ -21,12 +21,23 @@ describe('PO Parser', () => {
 
       expect(parsed).to.deep.equal(JSON.parse(json));
     });
+
+    it('should parse all known headers', async () => {
+      const [po, json] = await Promise.all([
+        readFile(path.join(__dirname, 'fixtures/headers-known.po')),
+        readFile(path.join(__dirname, 'fixtures/headers-known.json'), 'utf8')
+      ]);
+
+      const parsed = gettextParser.po.parse(po);
+
+      expect(parsed).to.deep.equal(JSON.parse(json));
+    });
   });
 
   describe('UTF-8', () => {
     it('should parse', async () => {
       const [po, json] = await Promise.all([
-        readFile(path.join(__dirname, 'fixtures/utf8.po')),
+        readFile(path.join(__dirname, 'fixtures/utf8.po'), 'utf8'),
         readFile(path.join(__dirname, 'fixtures/utf8-po.json'), 'utf8')
       ]);
 
@@ -87,7 +98,7 @@ describe('PO Parser', () => {
     });
   });
 
-  describe('parsing errors', () => {
+  describe.only('parsing errors', () => {
     const invalidKeyError = /Error parsing PO data: Invalid key name/;
 
     it('should throw (unescaped quote)', async () => {
@@ -115,6 +126,42 @@ describe('PO Parser', () => {
         expect(error.message).to.match(invalidKeyError);
         done();
       });
+    });
+
+    it('should throw (an entry has too few plural forms)', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-too-few-plural-forms.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Plural forms range error/);
+    });
+
+    it('should throw (an entry has too many plural forms)', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-too-many-plural-forms.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Plural forms range error/);
+    });
+
+    it('should throw (an entry misses "msgid_plural")', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgid-plural.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Translation string range error/);
+    });
+
+    it('should throw (an entry misses single "msgstr")', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgstr.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Translation string range error/);
+    });
+
+    it('should throw (duplicate entries found in the same context)', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-context-duplicate-entries.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Duplicate msgid error: entry "o1-1" in "c2"/);
+    });
+
+    it.only('should throw (an entry with multiple "msgid_plural")', async () => {
+      const po = await readFile(path.join(__dirname, 'fixtures/validate-redundant-msgid-plural.po'));
+
+      expect(gettextParser.po.parse.bind(gettextParser.po, po)).to.throw(/Multiple msgid_plural error: entry "o1-1"/);
     });
   });
 });
