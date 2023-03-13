ea4179b7021f85bfe7d20c8bdf3135f07d770396
Volodymyr Khytskyi <83341060+vkhytskyi-allegro@users.noreply.github.com>
2023-03-13 15:34:06 +0100

    review comments -- adjusted argument parsing logic

--- test/po-parser-test.js
+++ test/po-parser-test.js
@@ -3,6 +3,7 @@ const { promisify } = require('util');
 const path = require('path');
 const fs = require('fs');
 const gettextParser = require('..');
+const { normalizeParseArgs, normalizeStreamArgs } = require('../lib/poparser');
 
 const readFile = promisify(fs.readFile);
 
@@ -10,6 +11,76 @@ const expect = chai.expect;
 chai.config.includeStack = true;
 
 describe('PO Parser', () => {
+  describe('normalizeParseArgs', () => {
+    [
+      {
+        input: ['foo'],
+        expected: { buffer: 'foo', defaultCharset: undefined, validation: undefined }
+      },
+      {
+        input: ['foo', 'bar'],
+        expected: { buffer: 'foo', defaultCharset: 'bar' }
+      },
+      {
+        input: ['foo', { defaultCharset: 'bar' }],
+        expected: { buffer: 'foo', defaultCharset: 'bar', validation: undefined }
+      },
+      {
+        input: ['foo', { validation: true }],
+        expected: { buffer: 'foo', defaultCharset: undefined, validation: true }
+      },
+      {
+        input: ['foo', { defaultCharset: 'bar', validation: true }],
+        expected: { buffer: 'foo', defaultCharset: 'bar', validation: true }
+      }
+    ].forEach(({ input, expected }) => {
+      it(`should return normalized arguments for argument list of ${JSON.stringify(input)}`, () => {
+        expect(normalizeParseArgs(...input)).to.deep.equal(expected);
+      });
+    });
+  });
+
+  describe('normalizeStreamArgs', () => {
+    [
+      {
+        input: [],
+        expected: { transformOptions: {} }
+      },
+      {
+        input: ['foo'],
+        expected: { defaultCharset: 'foo', transformOptions: {} }
+      },
+      {
+        input: ['foo', { initialTreshold: 123 }],
+        expected: { defaultCharset: 'foo', transformOptions: { initialTreshold: 123 } }
+      },
+      {
+        input: [{ initialTreshold: 123 }],
+        expected: { transformOptions: { initialTreshold: 123 } }
+      },
+      {
+        input: [{ validation: true }],
+        expected: { defaultCharset: undefined, validation: true, transformOptions: {} }
+      },
+      {
+        input: [{ defaultCharset: 'foo' }],
+        expected: { defaultCharset: 'foo', validation: undefined, transformOptions: {} }
+      },
+      {
+        input: [{ defaultCharset: 'foo', validation: true }],
+        expected: { defaultCharset: 'foo', validation: true, transformOptions: {} }
+      },
+      {
+        input: [{ defaultCharset: 'foo', validation: true }, { initialTreshold: 123 }],
+        expected: { defaultCharset: 'foo', validation: true, transformOptions: { initialTreshold: 123 } }
+      }
+    ].forEach(({ input, expected }) => {
+      it(`should return normalized arguments for argument list of ${JSON.stringify(input)}`, () => {
+        expect(normalizeStreamArgs(...input)).to.deep.equal(expected);
+      });
+    });
+  });
+
   describe('headers', () => {
     it('should detect charset in header', async () => {
       const [po, json] = await Promise.all([
@@ -117,106 +188,106 @@ describe('PO Parser', () => {
     });
 
     describe('when validation is disabled', () => {
-      const validation = false;
+      const options = { validation: false };
 
       it('should throw (unescaped quote)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/error-unescaped-quote.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw(invalidKeyError);
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw(invalidKeyError);
       });
 
       it('should throw (double-escaped quote)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/error-double-escaped-quote.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw(invalidKeyError);
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw(invalidKeyError);
       });
 
       it('should not throw (an entry has too few plural forms)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-too-few-plural-forms.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
 
       it('should not throw (an entry has too many plural forms)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-too-many-plural-forms.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
 
       it('should not throw (an entry misses "msgid_plural")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgid-plural.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
 
       it('should not throw (an entry misses single "msgstr")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgstr.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
 
       it('should not throw (duplicate entries found in the same context)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-context-duplicate-entries.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
 
       it('should not throw (an entry with multiple "msgid_plural")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-redundant-msgid-plural.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.not.throw();
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.not.throw();
       });
     });
 
     describe('when validation is enabled', () => {
-      const validation = true;
+      const options = { validation: true };
 
       it('should throw (unescaped quote)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/error-unescaped-quote.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw(invalidKeyError);
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw(invalidKeyError);
       });
 
       it('should throw (double-escaped quote)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/error-double-escaped-quote.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw(invalidKeyError);
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw(invalidKeyError);
       });
 
       it('should throw (an entry has too few plural forms)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-too-few-plural-forms.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Plural forms range error: Expected to find 3 forms but got 2 for entry "o1-2" in "" context.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Plural forms range error: Expected to find 3 forms but got 2 for entry "o1-2" in "" context.');
       });
 
       it('should throw (an entry has too many plural forms)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-too-many-plural-forms.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Plural forms range error: Expected to find 2 forms but got 3 for entry "o1-2" in "" context.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Plural forms range error: Expected to find 2 forms but got 3 for entry "o1-2" in "" context.');
       });
 
       it('should throw (an entry misses "msgid_plural")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgid-plural.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Translation string range error: Extected 1 msgstr definitions associated with "o1-1" in "" context, found 2.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Translation string range error: Extected 1 msgstr definitions associated with "o1-1" in "" context, found 2.');
       });
 
       it('should throw (an entry misses single "msgstr")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-missing-msgstr.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Translation string range error: Extected 1 msgstr definitions associated with "o1" in "" context, found 0.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Translation string range error: Extected 1 msgstr definitions associated with "o1" in "" context, found 0.');
       });
 
       it('should throw (duplicate entries found in the same context)', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-context-duplicate-entries.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Duplicate msgid error: entry "o1-1" in "c2" context has already been declared.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Duplicate msgid error: entry "o1-1" in "c2" context has already been declared.');
       });
 
       it('should throw (an entry with multiple "msgid_plural")', async () => {
         const po = await readFile(path.join(__dirname, 'fixtures/validate-redundant-msgid-plural.po'));
 
-        expect(gettextParser.po.parse.bind(gettextParser.po, po, validation)).to.throw('Multiple msgid_plural error: entry "o1-1" in "" context has multiple msgid_plural declarations.');
+        expect(gettextParser.po.parse.bind(gettextParser.po, po, options)).to.throw('Multiple msgid_plural error: entry "o1-1" in "" context has multiple msgid_plural declarations.');
       });
     });
   });
