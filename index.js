f3d5cf8e68cc4f9dd9b0ed7e052852373b62b365
Erik Golinelli <erik@codekraft.it>
2024-05-04 23:54:01 +0200

    Add Typescript for type checking (but still with js files) (#81)
    * Emit types using tsc

--- index.js
+++ index.js
@@ -3,12 +3,24 @@ import poCompiler from './lib/pocompiler.js';
 import moParser from './lib/moparser.js';
 import moCompiler from './lib/mocompiler.js';
 
+/**
+ * Translation parser and compiler for PO files
+ * @see https://www.gnu.org/software/gettext/manual/html_node/PO.html
+ *
+ * @type {import("./index.d.ts").po} po
+ */
 export const po = {
   parse: poParser.parse,
   createParseStream: poParser.stream,
   compile: poCompiler
 };
 
+/**
+ * Translation parser and compiler for PO files
+ * @see https://www.gnu.org/software/gettext/manual/html_node/MO.html
+ *
+ * @type {import("./index.d.ts").mo} mo
+ */
 export const mo = {
   parse: moParser,
   compile: moCompiler
