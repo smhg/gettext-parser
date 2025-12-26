1d6872682031ffe57416eb8688cb9ab89bcd2a3c
Damian Krzeminski <pirxpilot@furkot.com>
2025-12-26 08:54:03 -0700

    enforce `import/enforce-node-protocol-usage` eslint rule

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,13 @@ import js from '@eslint/js';
 import globals from 'globals';
 import { defineConfig } from 'eslint/config';
 import stylistic from '@stylistic/eslint-plugin';
+import importPlugin from 'eslint-plugin-import';
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: {
+      import: importPlugin,
       js,
       '@stylistic': stylistic
     },
@@ -17,6 +19,9 @@ export default defineConfig([
         ...globals.node,
         ...globals.mocha
       }
+    },
+    rules: {
+      'import/enforce-node-protocol-usage': ['error', 'always']
     }
   }
 ]);
